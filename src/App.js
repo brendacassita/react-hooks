3f1e1c39041e935ee9b16c4d27d8337a9b939f97
Brenda <brendacassita@gmail.com>
2022-03-18 16:50:53 -0600

    react hooks class done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Counter from './class/Counter';
 
 function App() {
+  const [showThings, setShowThings] = useState(true)
+  const [toggleCount, setToggleCount] = useState(0)
+
+  
+  // show component did unmount cb
+  const renderThings = ()=>{
+    return (
+      <div>
+        {/* giving someNumber prop to {toggleCount} changes state in app.js */}
+        {/* sending new props causes it to rerender 
+        and calls render and componentDidUpdate methods */}
+        <Counter someNumber={toggleCount}/>
+      </div>
+    )
+  }
+
+  const toggle = ()=>{
+    setToggleCount(toggleCount +1)
+    // setShowThings(!showThings) comment out to not unmount/mount to only update count
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{border:'5px solid hotpink'}}>
+      <h1> App Component</h1>
+
+
+      <p>toggled {toggleCount} times</p>
+      {/* <button onClick={toggle}>toggle things()</button> */}
+      <button onClick={toggle}>update toggleCount in App.js</button>  
+      <Counter someNumber={toggleCount}/>
+      <hr/>
+
+
+      {/* {showThings && renderThings()} */}
+      {/* if this is true calling function and render my things, if not i wont */}
+      {/* setting state showThings to render components (add/remove to DOM) */}
+      {/* <button onClick={()=>setShowThings(!showThings)}>toggle</button> */}
+      
     </div>
   );
 }
---
+++ src/class/Counter.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+// classs
+
+
+class Counter extends React.Component{
+    // 1. (order to be called in)
+    // class thing where we call constructor in React component
+    constructor(props){
+        super(props)
+        console.log('Counter constructor called')
+        this.state = {count:0}
+    }
+
+    // 3. (order to be called in)
+    componentDidMount(){
+        console.log('Counter componentDidMount called')
+        console.log('-----MOUNTING PHASE DONE-----')
+    }
+
+    // after component rerendered
+    componentDidUpdate(prevProps, prevState){
+        console.log('Counter componentDidUpdate called')
+        console.log('prevState:', prevState)
+        console.log('prevProps:', prevProps)
+
+        if(prevState.count === 10){     // checking if count is 10, then setState 
+          this.setState({count: 0})     // so there is not an infinite loop, once it 
+                                        // gets to 10 it updae
+        }
+    }
+
+
+    increment = ()=>{
+        let newCount = this.state.count +1
+        console.log('Counter setState method called')
+        this.setState({count: newCount})
+    }
+
+
+    componentWillUnmount(){
+        console.log('Counter componentWillUnmount called')
+        console.log('-----NO LONGER ON DOM: UNMOUNTED-----')
+    }
+
+    
+    // 2. (order to be called in)
+    // class componenent needs render function before returning JSX
+    render(){
+        console.log('Counter render called')
+        return(
+            <div style={{border:'3px solid pink'}}>
+                <h1>Counter Component</h1>
+                <h1>Counter:</h1>
+                <p>count state in Counter: {this.props.someNumber}</p>
+                <p>someNumber prop: {this.state.count}</p>
+                <button onClick={this.increment}>add</button>
+            </div>
+        )
+    }
+}
+
+export default Counter
